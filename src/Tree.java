6a2074ff36030e4a9f970cb7837a71bd89d5293a
Jonathan <johnnny.chung@gmail.com>
2024-09-16 20:59:15 -0400

    constructor method

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tree {
-    // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+
+    private int _root;
+    private List<Tree> _subtrees;
+
+    public Tree(int _root, List<Tree> _subtrees) {
+        this._root = _root;
+
+        if (_subtrees == null)
+        {
+            this._subtrees = new ArrayList<>();
+        } else {
+            this._subtrees = new ArrayList<>(_subtrees);
+        }
+
+    }
+
 }
