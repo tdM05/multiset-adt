2613f002ac1029ff6a02481ffc6cb7c2408581eb
mousa-sururi1 <mousa.sururi@mail.utoronto.ca>
2024-09-16 20:59:45 -0400

    A

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,17 @@
 public class Tree {
+    public class __len__ {
+    /// Return the number of items contained in this tree.
+    if ..... {
+        return 0
+    } else {
+        size = 1
+        for ...... {
+            size += .....
+            }
+        return size
+        }
+
+    }
+
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
 }
