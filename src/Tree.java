490f097bceb325c16c5195e55afcb028146302ac
tdM05 <taddmao@gmail.com>
2024-09-16 20:53:53 -0400

    implemeneted isempty

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,5 @@
 public class Tree {
-    // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+    public boolean is_empty() {
+        return _subtrees.length() == 0;
+    }
 }
